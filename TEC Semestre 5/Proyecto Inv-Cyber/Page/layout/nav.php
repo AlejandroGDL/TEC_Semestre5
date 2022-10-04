a7ee4ba75998c96af403dc8baa2774bcd4bc11c2
Alejandro Guevara de Luna <alejandrogdlcecyt@gmail.com>
2022-10-04 09:59:02 -0500

    Cambios Page
    Cambios realizado en la Page

--- TEC Semestre 5/Proyecto Inv-Cyber/Page/layout/nav.php
+++ TEC Semestre 5/Proyecto Inv-Cyber/Page/layout/nav.php
@@ -1,6 +1,11 @@
-<nav class="nav"> 
-<button>Ventas <img src="icons/ShopCar_Dark.png" alt=""></button>
-<button>Agregar <img src="icons/Add_Dark.png" alt=""></button>
-<button>Editar <img src="icons/Edit_Dark.png" alt=""></button>
-<button>Eliminar <img src="icons/Delete_Dark.png" alt=""></button>
+<nav> 
+    <div class="nav">
+        <button>Ventas <img src="icons/ShopCar_Dark.png" alt=""></button>
+        <a></a>
+        <button>Agregar <img src="icons/Add_Dark.png" alt=""></button>
+        <button>Editar <img src="icons/Edit_Dark.png" alt=""></button>
+        <a></a>
+        <button>Eliminar <img src="icons/Delete_Dark.png" alt=""></button>
+    </div>
+    <a class="Divider">Ayuda</a>
 </nav>

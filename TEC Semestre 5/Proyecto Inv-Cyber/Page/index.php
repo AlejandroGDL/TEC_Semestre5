300fc3727ea811dc000e41b852e01c0216636db4
Alejandro Guevara de Luna <alejandrogdlcecyt@gmail.com>
2022-10-07 22:48:12 -0500

    Index Full
    Index Terminado

--- TEC Semestre 5/Proyecto Inv-Cyber/Page/index.php
+++ TEC Semestre 5/Proyecto Inv-Cyber/Page/index.php
@@ -5,10 +5,51 @@
     </head>
 
     <body class="grid-container">
+        <!--Encabezado Pagina-->
         <?php require('layout/header.php')?>
+        <!--Barra Navegación Pagina-->
         <?php require('layout/nav.php')?>
-        <article class="main">Main</article>
-        <footer class="footer">Footer</footer>
+        <!--Cuerpo Pagina-->
+        <article class="main">
+            
+            <!--Botones navegación Main-->
+            <div class="Main_Menu">
+                <p>Clave del Producto:</p>
+                <input type="text" placeholder="Código de barras" class="txtSearch"></input>
+                <button class="Enter">Enter</button>
+                <button Class="Search">Buscar <img src="Icons/Search_White.png"></button>
+            </div>
+        
+            <table>
+                <!--Encabezado -->
+                <thead class="Encabezado">
+                    <tr>
+                        <th>Clave</th>
+                        <th>Descripción</th>
+                        <th>Cantidad</th>
+                        <th>Precio</th>
+                        <th>Importe</th>
+                    </tr>
+                </thead>
+
+                <!--Cuerpo Tabla-->
+                <tbody>
+                    <th>5</th>
+                    <th>Producto de ejemplo 5</th>
+                    <th>2</th>
+                    <th>19.99</th>
+                    <th>39.98</th>
+                </tbody>
+            </table>
+
+        </article>
+
+        <!--Píe de Pagina-->
+        <footer class="footer">
+            <p>$999.99</p>
+            <button class="btnCobrar">Cobrar</button>
+        </footer>
+
     </body>
 
 </html>
